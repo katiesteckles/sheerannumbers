700305b528a2997c1603b942c28fb6b60c66b04e
katiesteckles <katiesteckles@gmail.com>
2017-10-31 20:12:39 +0000

    Added code comments

--- sheeran.py
+++ sheeran.py
@@ -1,6 +1,7 @@
-import itertools
-import csv
-albums=[1.0,3.0,4.0,4.0,5.0,7.0,13.0,13.0,18.0,19.0,21.0,25.0,25.0,50.0,1977.0,1989.0,8701.0]
+import itertools # for generating list of permutations
+import csv # for output
+albums=[1.0,3.0,4.0,4.0,5.0,7.0,13.0,13.0,18.0,19.0,21.0,25.0,25.0,50.0,1977.0,1989.0,8701.0] # .0 forces floats
+# initialise lists
 ptd=[]
 pdt=[]
 dpt=[]
@@ -8,9 +9,9 @@ dtp=[]
 tpd=[]
 tdp=[]
 
-perms = list(itertools.permutations(albums,4))
+perms = list(itertools.permutations(albums,4)) # generate all possible orderings of 4 things from the 17
 
-for i in perms:
+for i in perms: # iterate over all the sets of four things and work out all the different numbers using the three symbols
 	ptd.append(i[0]+(i[1]*(i[2]/i[3])))
 	pdt.append(i[0]+((i[1]/i[2])*i[3]))
 	dpt.append((i[0]/i[1])+(i[2]*i[3]))
@@ -18,7 +19,8 @@ for i in perms:
 	tpd.append((i[0]*i[1])+(i[2]/i[3]))
 	tdp.append((i[0]*(i[1]/i[2]))+i[3])
 
+# output to csv
 with open('sheeran-numbers.csv', 'wb') as csvfile:
     sheeran = csv.writer(csvfile, delimiter=',')
     for i in range(len(perms)):
-		sheeran.writerow(list(perms[i]) + [ptd[i], pdt[i], dtp[i], dpt[i], tdp[i], tpd[i]])
+		sheeran.writerow(list(perms[i]) + [ptd[i], pdt[i], dtp[i], dpt[i], tdp[i], tpd[i]]) # include the four albums used also
